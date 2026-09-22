5a2a598d56320c39fd1d1fc345c715edc1ebaf40
Luiz Eduardo Melo <edulu123419@gmail.com>
2026-09-22 16:26:35 -0300

    Update database.js

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,3 +1,18 @@
+// api
+
+// npm install express mongoose cors dotenv
+// npm install -D typescript @types/express @types/cors @types/node ts-node-dev
+//  npm install bcryptjs jsonwebtoken
+
+// ui
+
+// npm install
+// npm install react-router-dom axios
+// npm install -D tailwindcss postcss autoprefixer
+// npm install lucide-react
+// npm install -D sass
+
+
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
